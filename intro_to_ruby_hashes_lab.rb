4d92ad4b7bce249b309b963c7f2034169b8dd058
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-04 06:08:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,6 +32,7 @@ def monopoly_with_third_tier
       :pieces => 4,
       :names => {
         :reading_railroad: => {},
+        
       },
       :rent_in_dollars => {
         one_piece_owned => 25,
